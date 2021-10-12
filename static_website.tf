0d0c4b10fd943e0c3560d96f15e670fb58d0e7be
EC2 Default User <ec2-user@ip-172-31-7-16.eu-north-1.compute.internal>
2021-10-12 08:43:43 +0000

    test2

--- static_website.tf
+++ static_website.tf
@@ -3,6 +3,6 @@ module "static-site" {
   version           = "3.0.0"
   bucket_versioning = true
   hosted_zone_name  = "thecloudcollege.com"
-  name_prefix       = "var.student_name"
+  name_prefix       = var.student_name
   site_name         = "ceiv001.thecloudcollege.com"
 }
